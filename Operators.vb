f2aea44355828acf690c71d953bb5e871140fdd1
Brayden Peck <brayden.peck2@gmail.com>
2021-01-13 08:34:59 -0700

    Add project files.

---
+++ Operators.vb
@@ -0,0 +1,14 @@
+﻿Module Operators
+
+    Sub Main()
+        Console.WriteLine(2 + 2) 'Add
+        Console.WriteLine(8 - 2) 'Subtract
+        Console.WriteLine(5 * 5) 'Multiply
+        Console.WriteLine(1 / 2) 'Regular Div
+        Console.WriteLine(1 \ 2) 'Integer Div
+        Console.WriteLine(1 Mod 2) 'Division returns only the remainder
+        Console.ReadLine()
+
+    End Sub
+
+End Module
